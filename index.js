7a93cff302e200c3bb36a8d7cec4a3a4aa28a720
Sara Hopper <sara.hopper@gmail.com>
2017-09-09 16:57:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ function showRepositories(event, data) {
   console.log(this.responseText)
   let repoList = "<ul>"
   for(var i=0;i < this.responseText.length; i++) {
-    repoList += "<li>" + this.responseText[i]["name"] +
-    "</li>"
+    repoList += "<li>" + this.responseText[i]["name"] + "</li>"
   }
 }
 
