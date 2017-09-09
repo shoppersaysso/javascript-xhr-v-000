7b39219f440b8578fb5a979c8d0ad686ff2dbfb0
Sara Hopper <sara.hopper@gmail.com>
2017-09-09 16:51:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ function showRepositories(event, data) {
   console.log(this.responseText)
 }
 
-
 function getRepositories() {
   const req = new XMLHttpRequest()
   req.open("GET",
   'https://api.github.com/users/shoppersaysso/repos')
   req.send()
 }
-
