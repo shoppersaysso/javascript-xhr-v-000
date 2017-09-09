809cade2b0585869b4f9daffa7a2e40fc271b2d8
Sara Hopper <sara.hopper@gmail.com>
2017-09-09 17:06:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}<ul>`
-  for(var i=0;i < this.responseText.length; i++) {
-    repoList += "<li>" + this.responseText[i]["name"] + "</li>"
-  }
-  repoList += "</ul>"
   document.getElementById("repositories").innerHTML = repoList
 }
 
