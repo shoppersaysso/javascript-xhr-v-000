6aec2e9edd66c6f03da9b5fd1cd13b7a72a240d5
Sara Hopper <sara.hopper@gmail.com>
2017-09-09 17:19:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="'+ r.name +
-  '"onclick="getCommits(this)">Get Commits</a></li>').join('')}<ul>`
+'"onclick="getCommits(this)">Get Commits</a></li>').join('')}<ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
 
@@ -17,7 +17,10 @@ function getCommits(el) {
 
 function showCommits() {
   const commits = JSON.parse(this.responseText)
-  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + 
+commit.author.login + '</strong> - ' + commit.commit.message + 
+'</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML = commitsList
 }
 
 function getRepositories() {
