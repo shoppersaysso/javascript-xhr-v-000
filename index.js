b57822eab2b72fca145effa52ca5da2f69b0595b
Sara Hopper <sara.hopper@gmail.com>
2017-09-09 16:45:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getRepositories() {
   const req = new XMLHttpRequest()
   req.open("GET",
-  'https://api.github.com/users/shoppersaysso/repos')
+  'https://api.github.com/users/octocat/repos')
   req.send()
 }
